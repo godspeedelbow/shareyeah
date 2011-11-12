51db569dd75c0009e74da39eb8d06ee7c978db57
jmatser <jonas@jonasmatser.nl>
2011-11-12 23:15:26 +0100

    Removed render arguments in Application.index

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,10 +10,7 @@ import models.*;
 public class Application extends AbstractController {
 
     public static void index() {
-    	String IP = Http.Request.current().remoteAddress;
-    	
-    	List<Note> notes = Note.find("order by postedAt desc").fetch();
-        render(IP, notes);
+        render();
     }
 
 }
