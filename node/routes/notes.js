42c5d7a6705f64adbc2ebabf8a6496f2713ff745
godspeedelbow <eelke@boezeman.com>
2013-01-28 23:31:34 +0100

    added dummy data and mongoose dep

--- node/routes/notes.js
+++ node/routes/notes.js
@@ -1,5 +1,16 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
 module.exports.get = function(req, res) {
-    res.send([{name:'wine1'}, {name:'wine2'}]);
+    res.send([{
+        id: 1, 
+        content:'http://soundcloud.com/world-records-dnb/drum-and-bass-dazer-destiny</a> Some drum n bass song hosted on SoundCloud',
+        created: 'Some time ago'
+    }, {
+        id: 2,
+        content:'<a href="http://www.youtube.com/watch?v=ZbcgyPtYBY0">http://www.youtube.com/watch?v=ZbcgyPtYBY0</a>',
+        created: 'Some time ago'
+    }]);
 };
 
 // module.exports.create = function(req, res) {
